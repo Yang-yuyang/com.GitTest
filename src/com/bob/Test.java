e62454bf99d9aeedcf60874fe82ede61ebbe9439
Yang-yuyang <923089009@qq.com>
2021-12-21 09:42:46 +0800

    Initial commit

---
+++ src/com/bob/Test.java
@@ -0,0 +1,13 @@
+package com.bob;
+
+/**
+ * @author bob_Uba
+ * @date 2021/12/21 - 9:41
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("你好！");
+    }
+
+}
